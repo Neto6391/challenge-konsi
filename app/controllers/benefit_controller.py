2eb151dcd2b08b8d42b9392cda649341731ddc13
Neto6391 <neto6391@gmail.com>
2023-08-02 16:30:55 -0300

    refactor in controller and services

--- app/controllers/benefit_controller.py
+++ app/controllers/benefit_controller.py
@@ -1,8 +1,18 @@
 from app.schemas.cpf_benefit_data import CPFBenefitData
 from app.services.benefit_service import BenefitService
+from config.logging import logger
 
 class BenefitController:
     benefit_service = BenefitService()
 
-    def create_benefit_by_cpf(self, data: CPFBenefitData):
-        return self.benefit_service.create_benefit_by_cpf(data)
+    def get_hello_callback_consumer(self, message):
+        logger.info(f"Hello -> input: {message}")
+
+    async def create_benefit_by_cpf(self, data: CPFBenefitData):
+        try:
+            await self.benefit_service.initialize()
+            await self.benefit_service.add_queue_cpf_benefit_list_async(data.cpf)
+            await self.benefit_service.process_cpf_benefit_queue_async(input_callback=self.get_hello_callback_consumer)
+        except Exception as e:
+            logger.error(f'Found error in processing queues: {e}')
+            raise
--- app/services/benefit_service.py
+++ app/services/benefit_service.py
@@ -1,7 +1,41 @@
 
-from app.schemas.cpf_benefit_data import CPFBenefitData
+import asyncio
+from typing import List
+
+
+from app.producer.producer import Producer
+from app.consumer.consumer import Consumer
+from app.utils.rabbitmq_manager_factory import RabbitMQFactory
+from config.logging import logger
 
 class BenefitService:
+    def __init__(self):
+        self.rabbitmq_manager = None
+        self.producer_cpf = None
+        self.consumer_cpf = None
 
-    def create_benefit_by_cpf(self, data: CPFBenefitData):
-        return data
+    async def initialize(self):
+        self.rabbitmq_manager = await RabbitMQFactory.create_rabbitmq_manager()
+        self.producer_cpf = Producer(self.rabbitmq_manager, queue_name="cpf_queue")
+        self.consumer_cpf = Consumer(self.rabbitmq_manager, queue_name="cpf_queue")
+
+    async def add_queue_cpf_benefit_async(self, cpf: str):
+        try:
+            await self.producer_cpf.publish_message(cpf)
+        except Exception as e:
+            logger.error(f"Error adding CPF to queue: {e}")
+            raise
+        
+    async def add_queue_cpf_benefit_list_async(self, cpfList: List[str]):
+        try:
+
+            tasks = [self.add_queue_cpf_benefit_async(cpf) for cpf in cpfList]
+            await asyncio.gather(*tasks)
+        except Exception as e:
+            logger.error(f"Error adding CPFs to queue: {e}")
+            raise
+    
+    async def process_cpf_benefit_queue_async(self, input_callback):
+        await self.consumer_cpf.start_consuming(input_callback=input_callback)
+
+        
