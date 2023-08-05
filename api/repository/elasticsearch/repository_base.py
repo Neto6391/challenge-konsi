bab259960b692db56757bcf0a3700d495c705847
Neto6391 <neto6391@gmail.com>
2023-08-05 17:10:59 -0300

    add get benefits by cpf ou search all

--- api/repository/elasticsearch/repository_base.py
+++ api/repository/elasticsearch/repository_base.py
@@ -1,5 +1,6 @@
 from api.utils.elastic_search_factory import ElasticConfigFactory
 from config.elastic_search_config import ElasticConfig
+from config.logging import logger
 
 
 class RepositoryBase:
@@ -11,6 +12,14 @@ class RepositoryBase:
             result = await self.client.create_index(index=index_name, id=id, document=data)
             return result
         except Exception as e:
-            print("Error indexing document: ", e)
+            logger.error(f'Error indexing document: {e}')
+            return None
+        
+    async def search_index(self, index, query, from_=None, size=None):
+        try:
+            results = await self.client.search(index=index, query=query, from_=from_, size=size)
+            return results
+        except Exception as e:
+            logger.error(f'Error searching in Elasticsearch: {e}')
             return None
 
--- api/routes.py
+++ api/routes.py
@@ -2,16 +2,31 @@ from fastapi import APIRouter, Depends
 from api.repository.benefit_repository import BenefitRepository
 from api.schemas.cpf_benefit_data import CPFBenefitData
 from api.controllers.benefit_controller import BenefitController
+from api.schemas.query_params_data import QueryParamsData
 from api.utils.rabbitmq_manager_singleton import RabbitMQManagerSingleton
 from api.utils.redis_cache_factory import RedisCacheFactory
+from config.logging import logger
 
 router = APIRouter()
 
 @router.post('/api/cpf/create')
 async def create_benefit_by_cpf(data: CPFBenefitData, rabbitmq_manager_singleton: RabbitMQManagerSingleton = Depends(RabbitMQManagerSingleton), redis_cache: RedisCacheFactory = Depends(RedisCacheFactory), benefit_repository: BenefitRepository = Depends(BenefitRepository)):
     try:
-        benefit_controller = BenefitController(rabbitmq_manager_singleton.get_rabbitmq_manager(), redis_cache.get_instance(), benefit_repository)
+        benefit_controller = BenefitController(benefit_repository, rabbitmq_manager_singleton.get_rabbitmq_manager(), redis_cache.get_instance())
         await benefit_controller.create_benefit_by_cpf(data)
-        return None
+        return "Ok"
+    except Exception as e:
+        raise ValueError(f'Something is happen {e}')
+    
+@router.get('/api/cpf')
+async def create_benefit_by_cpf(params: QueryParamsData = Depends(), benefit_repository: BenefitRepository = Depends(BenefitRepository)):
+    try:
+        benefit_controller = BenefitController(benefit_repository)
+        logger.info(f"Data ${params.cpf} - {params.page} - {params.page_size}")
+        if params.cpf:
+            return await benefit_controller.get_cpf_data_by_cpf(cpf=params.cpf)
+        else:
+            
+            return await benefit_controller.get_all_cpf_data(page=params.page, page_size=params.page_size)
     except Exception as e:
         raise ValueError(f'Something is happen {e}')
